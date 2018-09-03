af8b1af5e94ab84d501600a2a628a6b34677f90e
Daniel Hocking <daniel.hocking@gmail.com>
2018-09-03 10:19:37 +1000

    Allow sending of a file in segments

--- Receiver/receiver.py
+++ Receiver/receiver.py
@@ -12,9 +12,13 @@ server_port = int(sys.argv[1])
 receiver_socket = socket(AF_INET, SOCK_DGRAM)
 receiver_socket.bind(('', server_port))
 
-while 1:
-  sentence = receiver_socket.recv(1024)
-  print(sentence.decode())
-  if sentence.decode() == 'done':
-    receiver_socket.close()
-    break
+count = 1
+with open('file_r.pdf', mode='wb') as f:
+    while True:
+        segment = receiver_socket.recv(1024)
+        print(count)
+        count += 1
+        if segment == b'done':
+            receiver_socket.close()
+            break
+        f.write(segment)
--- Sender/sender.py
+++ Sender/sender.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3.6
 
 import sys
-from time import time
+from time import time, sleep
 from socket import *
 
-if len(sys.argv) != 3:
-    print('python sender.py host port (host and port must be \
+if len(sys.argv) != 4:
+    print('python sender.py host port filename (host, port and filename must be \
 provided as arguments')
     sys.exit()
 
 receiver_name = str(sys.argv[1])
 receiver_port = int(sys.argv[2])
+filename = sys.argv[3]
+max_seg_size = 1000
 sender_socket = socket(AF_INET, SOCK_DGRAM)
 sender_socket.settimeout(1)
 sender_socket.connect((receiver_name, receiver_port))
 
-for i in range(10):
-    sent_time = time()
-    message = f'PING {i} {sent_time}\n'
-    sender_socket.send(str.encode(message))
+count = 1
+with open(filename, mode='rb') as f:
+    while True:
+        segment_data = f.read(max_seg_size)
+        if not segment_data:
+            break
+        sender_socket.send(segment_data)
+        print(count)
+        count += 1
+        sleep(0.01)
+
 sender_socket.send(str.encode('done'))
 sender_socket.close()
 
